c3574969902fd436c0684d735d9c0ad850ec0580
ak2ume <ak2ume@users.noreply.github.com>
2022-07-03 18:16:04 +0900

    add config.fish

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+# set incremental search tool in ghq
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# peco 
+set fish_plugins theme peco
+
+# display git branch
+set -g theme_display_git_master_branch yes
+# prompt after newline
+set -g theme_newline_cursor yes
+# display date
+set -g theme_display_date no
+
+# Keybind
+function fish_user_key_bindings
+    bind \cr peco_select_history # Bind for peco history to Ctrl+r
+end
+
+# set incremental search tool in ghq
+set GHQ_SELECTOR peco
