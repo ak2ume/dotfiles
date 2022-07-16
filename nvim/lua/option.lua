537b07d5dc5bb046f9bc49e5ef15e63926da1f77
ak2ume <ak2ume@users.noreply.github.com>
2022-07-16 17:50:54 +0900

    add neovim configuration

---
+++ nvim/lua/option.lua
@@ -0,0 +1,17 @@
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.splitright = true
+vim.o.termguicolors = true
+vim.o.hidden = true
+vim.o.updatetime = 300
+vim.o.termguicolors = true
+vim.bo.expandtab = true
+vim.bo.autoindent = true
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.autoread = true
+vim.wo.number = true
+vim.wo.signcolumn = 'yes'
+vim.wo.cursorline = true
+vim.cmd 'set clipboard+=unnamedplus'
+
