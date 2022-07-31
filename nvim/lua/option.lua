b7866b43d149414f94c34a0c2901d2d526704d47
ak2ume <ak2ume@users.noreply.github.com>
2022-07-31 21:34:17 +0900

    add key bindings in Terminal mode

--- nvim/lua/option.lua
+++ nvim/lua/option.lua
@@ -14,3 +14,13 @@ vim.wo.signcolumn = 'yes'
 vim.wo.cursorline = true
 vim.cmd 'set clipboard+=unnamedplus'
 
+-- key bindings --
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+map("t", "<Esc>", "<C-Bslash><C-n>")
+map("t", "<C-[>", "<C-Bslash><C-n>")
+map("t", "<C-v><Esc>", "<Esc>")
+
